1c93d3cbc328c8ccc22b73a599f3c01d38a00cfa
Erwin Aguero <enaguero@gmail.com>
2026-02-23 18:06:21 +0000

    refactor: improve react patterns and code quality for landing page

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,15 +1,13 @@
 export const Card = (props) => {
 
     return(
-        <div className="col-3">
-            <button onClick={()=> console.log(props) }  className="btn btn-primary"/>
-            <div className="card w-100">
-                <button onClick={()=> console.log("")}></button>
-                <img src={props.imagen} className="card-img-top" alt="..." />
+        <div className="col-md-6 col-lg-3 mb-4">
+            <div className="card h-100 w-100">
+                <img src={props.imagen} className="card-img-top" alt={props.titulo} />
                 <div className="card-body">
                     <h5 className="card-title">{props.titulo} </h5>
                     <p className="card-text"> {props.descripcion} </p>
-                    <a target="_blank" href={props.url} className="btn btn-primary">Ver más</a>
+                    <a target="_blank" rel="noreferrer" href={props.url} className="btn btn-primary">Ver más</a>
                 </div>
             </div>
         </div>
